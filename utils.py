f747ff616413a90a60af5b42b04ab2bc3abd6f86
shaked571 <shakedgreenfeld@gmail.com>
2021-03-26 23:52:01 +0300

    fix path and other fixes

--- utils.py
+++ utils.py
@@ -1,18 +1,22 @@
 # This file provides code which you may or may not find helpful.
 # Use it if you want, or ignore it.
-import random
+import os
 def read_data(fname):
     data = []
-    for line in file(fname):
-        label, text = line.strip().lower().split("\t",1)
-        data.append((label, text))
+    with open(fname) as f:
+        for line in f.readlines():
+            label, text = line.strip().lower().split("\t", 1)
+            data.append((label, text))
     return data
 
+root_data_path = os.path.join(os.path.dirname(os.path.abspath((__file__))), 'data')
+train_p = os.path.join(root_data_path, 'train')
+dev_p = os.path.join(root_data_path, 'dev')
 def text_to_bigrams(text):
     return ["%s%s" % (c1,c2) for c1,c2 in zip(text,text[1:])]
 
-TRAIN = [(l,text_to_bigrams(t)) for l,t in read_data("train")]
-DEV   = [(l,text_to_bigrams(t)) for l,t in read_data("dev")]
+TRAIN = [(l,text_to_bigrams(t)) for l,t in read_data(train_p)]
+DEV = [(l,text_to_bigrams(t)) for l,t in read_data(dev_p)]
 
 from collections import Counter
 fc = Counter()
